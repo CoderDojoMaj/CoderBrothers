2de757ab529006655536f33ea578659f3ca484ac
ThePerkinrex <theperkinrex@gmail.com>
2020-03-02 11:10:11 +0100

    Fix routing so that css works

--- server.py
+++ server.py
@@ -1,14 +1,13 @@
-from flask import Flask, escape, request, redirect
+from flask import Flask, escape, request, redirect, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return redirect('/index.html')
-
-@app.route('/<page>')
+@app.route('/', defaults={'page': 'index.html'})
+@app.route('/<path:page>')
 def page(page):
     print(f'Loading {page}')
-    return open('web/'+page).read()
+    return send_from_directory(
+            'web/',
+            page)
 
 app.run(host='0.0.0.0', port=8080)
