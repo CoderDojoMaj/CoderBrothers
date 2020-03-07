d8ada93f71172cb0d19371cf87d667eb7dabbb0b
theperkinrex <theperkinrex@gmail.com>
2020-03-07 11:53:07 +0100

    Update server to disable caching

--- server.py
+++ server.py
@@ -2,12 +2,13 @@ from flask import Flask, escape, request, redirect, send_from_directory
 
 app = Flask(__name__)
 
+# For developing. Disable chaching for non html files
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
 @app.route('/', defaults={'page': 'index.html'})
 @app.route('/<path:page>')
 def page(page):
     print(f'Loading {page}')
-    return send_from_directory(
-            'web/',
-            page)
+    return send_from_directory('web/', page)
 
-app.run(host='0.0.0.0', port=8080)
+app.run(host='localhost', port=8080)
