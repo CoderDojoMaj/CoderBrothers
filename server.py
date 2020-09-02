c3e339a86559916e64d1a27a4ae9be8a729bef2d
magnitopic <magnitopic@gmail.com>
2020-09-02 23:05:12 +0200

    Work started on index.html

--- server.py
+++ server.py
@@ -217,4 +217,4 @@ def utility_functions():
     return dict(console=printconsole)
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=8080)
+    app.run(host='localhost', port=8080, debug=True)
