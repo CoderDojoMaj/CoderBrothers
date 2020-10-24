a0680a4b48422f5ffdfdf397bac9310af3a60876
magnitopic <magnitopic@gmail.com>
2020-10-24 16:23:14 +0200

    Work on blog.html begins

---
+++ web/js/blog.js
@@ -0,0 +1,15 @@
+function onLogin() {
+  document.getElementById("logIn").style.display = "block";
+}
+
+function onSignIn() {
+  document.getElementById("signUp").style.display = "block";
+}
+  
+function offLogin() {
+  document.getElementById("logIn").style.display = "none";
+}
+
+function offSignIn() {
+  document.getElementById("signUp").style.display = "none";
+}
