aaf18b682e5c6a6c3e7823e9417cf59b6a1e8878
ThePerkinrex <theperkinrex@gmail.com>
2020-11-02 13:16:40 +0100

    Create css directory if not present

--- python/scsscompiler.py
+++ python/scsscompiler.py
@@ -1,4 +1,4 @@
-from os import path
+from os import path, mkdir
 from scss.compiler import compile_file
 import scss
 from python import logger
@@ -6,31 +6,33 @@ from python import rcssmin
 
 
 def compile(file):
-    log = logger.get('SCSS')
-    p = path.splitext(file)[0]
-    css_file = path.join('web/css', p + '.css')
-    scss_file = path.join('web/scss', p + '.scss')
-    c = False
-    if path.exists(css_file):
-        if path.getmtime(css_file) < path.getmtime(scss_file):
-            c = True
-    else:
-        c = True
+	log = logger.get('SCSS')
+	p = path.splitext(file)[0]
+	if not path.exists('web/css'):
+		mkdir('web/css')
+	css_file = path.join('web/css', p + '.css')
+	scss_file = path.join('web/scss', p + '.scss')
+	c = False
+	if path.exists(css_file):
+		if path.getmtime(css_file) < path.getmtime(scss_file):
+			c = True
+	else:
+		c = True
 
-    if c:
-        log.info(f'Recompiling {css_file}')
-        s = None
-        try:
-            s = compile_file(scss_file)
-        except scss.errors.SassError as e:
-            log.error(f'SCSS error on file {scss_file}:')
-            log.error(e)
-            s = e.to_css() + 'body > * {visibility: hidden}'
-        if s:
-            s = rcssmin.cssmin(s)
-            with open(css_file, 'w') as css:
-                # log.info(s)
-                css.write(s)
+	if c:
+		log.info(f'Recompiling {css_file}')
+		s = None
+		try:
+			s = compile_file(scss_file)
+		except scss.errors.SassError as e:
+			log.error(f'SCSS error on file {scss_file}:')
+			log.error(e)
+			s = e.to_css() + 'body > * {visibility: hidden}'
+		if s:
+			s = rcssmin.cssmin(s)
+			with open(css_file, 'w') as css:
+				# log.info(s)
+				css.write(s)
 
-    else:
-        log.info(f'{css_file} already compiled')
+	else:
+		log.info(f'{css_file} already compiled')
