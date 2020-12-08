935a6f30bbc9adce0d3b0c1045b6174cbf855426
ThePerkinrex <theperkinrex@gmail.com>
2020-12-08 12:00:29 +0100

    Update blog

--- python/blog.py
+++ python/blog.py
@@ -1,25 +1,28 @@
-import os, jinja2
+import os
+import jinja2
 from python import logger, utils
 from python.db import getDB
 
 l = logger.get('BLOG')
 
+
 def get_posts():
-	posts = []
-	for post in getDB().getPosts():
-		date = post['date']
-		post['date'] = utils.formatPostDate(date)
-		posts.append(post)
-	return posts
+    posts = []
+    for post in getDB().getPosts():
+        date = post['date']
+        post['date'] = utils.formatPostDate(date)
+        posts.append(post)
+    return posts
+
 
 def getPostTemplate(uuid):
-	post = getDB().getPost(uuid)
-	# l.info((post['title'], post['author'], post['date'], post['content']))
-	# l.info('''[%s %s %s %s]''' % (post['title'], post['author'], post['date'], post['content']))
-	template = '''{%% extends "post.html" %%}
+    post = getDB().getPost(uuid)
+    # l.info((post['title'], post['author'], post['date'], post['content']))
+    # l.info('''[%s %s %s %s]''' % (post['title'], post['author'], post['date'], post['content']))
+    template = '''{%% extends "post.html" %%}
 	{%% block title %%} %s {%% endblock %%}
 	{%% block author %%} %s {%% endblock %%}
 	{%% block date %%} %s {%% endblock %%}
 	{%% block markdown %%} %s {%% endblock %%}''' % (post['title'], post['author'], post['date'], post['content'])
-	# l.info(template)
-	return template
+    # l.info(template)
+    return template
