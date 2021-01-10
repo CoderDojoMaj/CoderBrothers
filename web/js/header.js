5674913b82423de94114beb9eea0199616d9383d
magnitopic <magnitopic@gmail.com>
2021-01-10 12:12:04 +0100

    New css for header

--- web/js/header.js
+++ web/js/header.js
@@ -1,10 +1,10 @@
 function _open() {
 	document.getElementById('menu').style.display = "flex";
-	/*document.getElementById('openMenu').style.paddingBottom= "4vh";*/
+	//document.getElementById('openMenu').style.paddingBottom= "4vh";
 	setTimeout(() => { document.getElementById("menu").style.width = "70vw"; }, 10);
 }
 function _close() {
 	document.getElementById('menu').style.width = "0";
-	/*document.getElementById('openMenu').style.paddingBottom= "none";*/
+	//document.getElementById('openMenu').style.paddingBottom= "none";
 	setTimeout(() => { document.getElementById("menu").style.display = "none"; }, 500);
 }
